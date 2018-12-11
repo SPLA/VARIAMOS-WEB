374f6237e22317dc94130b9c98a55e618d81c21c
DanielGara <yo@danielgara.com>
2018-12-11 08:29:41 -0500

    IMP: about and report sections improved

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,7 +1,20 @@
 <template>
   <div>
     <h1>{{ $t("about_team") }}</h1>
-    {{ $t("about_team_list") }}
+    <div class="team-info">
+      <img alt="" src="/static/images/team/daniel.jpg" class="pull-left">
+      <div class="pull-left">
+        <span class="team-name">{{ $t("about_team_daniel") }}</span>
+        <span class="team-post">{{ $t("about_team_daniel_desc") }}</span>
+      </div>
+    </div>
+    <div class="team-info">
+      <img alt="" src="/static/images/team/raul.jpg" class="pull-left">
+      <div class="pull-left">
+        <span class="team-name">{{ $t("about_team_raul") }}</span>
+        <span class="team-post">{{ $t("about_team_raul_desc") }}</span>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -13,4 +26,38 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.team-info{
+    overflow: hidden;
+    padding-top: 10px;
+}
+
+.team-info span {
+    display: block;
+    text-align: left;
+}
+
+.team-info img {
+    margin-right: 15px;
+    border: 1px solid #f5f5f5;
+    border-radius: 150px !important;
+    height: 75px;
+    padding: 3px;
+    width: 75px;
+}
+
+.pull-left {
+    float: left!important;
+}
+
+span.team-name {
+    color: black;
+    font-size: 16px;
+    font-weight: 300;
+    margin: 15px 0 7px;
+}
+
+span.team-post {
+    color: #656565;
+    font-size: 12px;
+}
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import Home from '@/components/Home'
 import About from '@/components/About'
 import Models from '@/components/Models'
+import Reports from '@/components/Reports'
 
 Vue.use(Router)
 
@@ -22,6 +23,11 @@ export default new Router({
       path: '/models/:type',
       name: 'Models',
       component: Models
+    },
+    {
+      path: '/reports',
+      name: 'Reports',
+      component: Reports
     }
   ]
 })
