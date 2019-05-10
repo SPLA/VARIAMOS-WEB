5b5a6b3df15349f452ed331be9254024045e8166
andres4588 <andreerazo@umariana.edu.co>
2019-05-10 11:32:13 -0500

    creacion modelo

--- src/assets/js/common/global_info.js
+++ src/assets/js/common/global_info.js
@@ -8,13 +8,15 @@
 export function getModelInfo(){
     var info =[];
     //list of graphical models
-    info["gmodels"]=["feature","component","binding_feature_component"];
+    info["gmodels"]=["feature","component","feedback","binding_feature_component"];
     //define feature model main info
     info["feature"]={projFolders:["Domain","Application","Adaptation"], shown_Elements:["root", "abstract", "concrete"], checkbox_enable:true};
     //define component model main info
     info["component"]={projFolders:["Domain"], shown_Elements:["component", "file"], checkbox_enable:false};
     //define binding model main info
     info["binding_feature_component"]={projFolders:["Domain"], shown_Elements:['concrete', 'component'], checkbox_enable:false};
+    //define feedback model main info
+    info["feedback"]={projFolders:["Domain"], shown_Elements:["component", "file", "summing_point"], checkbox_enable:false};
     return info;
 }
 
