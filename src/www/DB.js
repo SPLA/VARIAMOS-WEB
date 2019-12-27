784ae480ebc98db249f518cdc7d289fd62f3650a
harvey.medina <harvey.medina@quipux.com>
2019-12-27 09:11:16 -0500

    Se desvincula servidor rest

--- src/www/DB.js
+++
@@ -1,9 +0,0 @@
-/**
- * Author: Jhon H. Medina
- * Fecha: 05/08/2019
- */
-
-//Conexion con mongo db
-module.exports = {
-    DB: 'mongodb://variamos:RSEqfbe5ghR0sDquHO3Ms2wWOLmL7euLeVYo9krr9jqw150zGBd7IPk4bl6HqdkxEyGDmJC5Id79ZydmupHtGQ==@variamos.documents.azure.com:10255/?ssl=true'
-}
