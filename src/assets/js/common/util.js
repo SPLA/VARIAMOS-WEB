f800361b0c4cadc4501ee302475e2ee55a374027
Andres Danilo Patiño Velez <andresdanilopv@gmail.com>
2020-01-18 19:58:39 -0500

    cambios y nuevo modelo

--- src/assets/js/common/util.js
+++ src/assets/js/common/util.js
@@ -119,13 +119,182 @@ export function modalButton(text, functionToAppend){
 }
  
 export function downloadFile(filename, text){
-    var element = document.createElement('a');
+    let element = document.createElement('a');
     element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
     element.setAttribute('download', filename); 
     element.style.display = 'none';
     document.body.appendChild(element); 
     element.click(); 
     document.body.removeChild(element);
+}
+
+export function modalComponentInformation(structure, idx_cat, idx_subcat, cell, graph, handleResize){
+    let table = document.createElement('table');
+    const category_keys = Object.keys(structure);
+    const subcategory_keys = Object.keys(structure[category_keys[idx_cat]]);
+    const elements = structure[category_keys[idx_cat]][subcategory_keys[idx_subcat]];
+
+    elements.forEach(element => {
+        let tr = document.createElement('tr');
+        let td = document.createElement('td');
+        td.innerHTML=element.element_id;
+        tr.appendChild(td);
+
+        let input = null;
+
+        if(element.element_type === 'label'){
+            input = document.createElement('th');
+            input.innerHTML = element.element_txt !== undefined ? element.element_txt.bold() : '';
+            input.setAttribute('align', 'center');
+        }
+        else if(element.element_type === 'text'){
+            input = document.createElement('input');
+            input.size=48;
+            if(element.element_placeholder_txt !== undefined && element.value === null){
+                input.value = element.element_placeholder_txt;
+            } else {
+                input.value = element.value;
+            }
+            //Set up an event listener that modifies the cell's attributes
+            //when the input's text is changed.
+            //This function selects which part to change according to
+            //the field's type.
+            input.addEventListener('input', () => {
+                console.log(element.element_instance);
+                const field = graph.getModel().getCell(element.element_instance);
+                graph.getModel().beginUpdate();
+                try{
+					let edit = new mxCellAttributeChange(
+                            field, 
+                            ['attr_text', 'method_text'].includes(element.element_field_type) 
+                                ? 'label' : 'description',
+							input.value);
+                    graph.getModel().execute(edit);
+                    element.value = input.value;
+				}
+				finally{
+					graph.getModel().endUpdate();
+				}
+            });
+        }
+        else if(element.element_type === 'combo'){
+            input = document.createElement('select');
+            let index = 0;
+            element.element_items.forEach(optionText => {
+                const option = document.createElement('option');
+                option.value = index++;
+                option.text = optionText;
+                input.appendChild(option);
+            });
+        } else if(element.element_type === 'textarea'){
+            input = document.createElement('textarea');
+            input.cols = 50;
+            input.rows = 10;
+            if(element.value !== null){
+                input.value = element.value;
+            }
+            input.addEventListener('input', () => {
+                const cell = graph.getModel().getCell(element.element_instance);
+                graph.getModel().beginUpdate();
+                try{
+					let edit = new mxCellAttributeChange(
+                            cell, 
+                            'information',
+							input.value);
+                    graph.getModel().execute(edit);
+                    element.value = input.value;
+                    console.log('element.value :', element.value);
+				}
+				finally{
+					graph.getModel().endUpdate();
+				}
+            });
+        } else if(element.element_type === 'button') {
+            input = document.createElement('div');
+            const b1 = document.createElement('button');
+            b1.innerHTML = element.button1_txt !== undefined ? element.button1_txt.bold() : '';
+            b1.addEventListener('click', () => {
+                //Add field to class
+                //If attributes, get second child, otherwise get the third (for methods);
+                const field_container = cell.getChildAt(element.element_prefix === 'attribute' ? 1 : 2);
+                const n_children = field_container.getChildCount();
+
+                const field_type = element.element_prefix;
+                const doc_field = mxUtils.createXmlDocument();
+                const node_field = doc_field.createElement(field_type);
+                node_field.setAttribute('type', field_type);
+                node_field.setAttribute('label', element.element_prefix === 'attribute' ? '- attribute : type' : '- method()');
+                node_field.setAttribute('description', '');
+                const field = graph.insertVertex(field_container, null, node_field, 1, (20 * n_children) + 1, 98, 18,'fillColor=#FFFFFF;selectable=0;align=left;fontColor=black;strokeColor=none;');
+                field.setConnectable(false);
+
+                //Handle Class Resize
+                handleResize(null, {getProperty(_string) {return [cell];}});
+
+                const count = (structure.basic_information[element.element_prefix+'s'].length - 1)/2 + 1;
+                structure.basic_information[element.element_prefix+'s'].push(
+                    {
+                        element_id: element.element_prefix+`${count}-name`, 
+                        element_type: 'text',
+                        element_instance: field.getId(),
+                        element_field_type: element.element_prefix === 'attribute' ? 'attr_text' : 'method_text', 
+                        value: element.element_prefix === 'attribute' ? '- attribute : type' : '- method()'
+                    },
+                    {   element_id: element.element_prefix+`${count}-Description`, 
+                        element_type: 'text',
+                        element_instance: field.getId(),
+                        element_field_type: element.element_prefix === 'attribute' ? 'attr_desc' : 'method_desc',
+                        value: ''
+                    }
+                );
+                let main_modal_body = document.getElementById('main_modal_body');
+                main_modal_body.innerHTML="";
+                let c_body = modalComponentInformation(structure, idx_cat, idx_subcat, cell, graph, handleResize);
+                main_modal_body.appendChild(c_body);
+            });
+            const b2 = document.createElement('button');
+            b2.innerHTML = element.button2_txt !== undefined ? element.button2_txt.bold() : '';
+            b2.addEventListener('click', () => {
+                if(structure.basic_information[element.element_prefix+'s'].length > 1){
+                    structure.basic_information[element.element_prefix+'s'].pop();
+                    structure.basic_information[element.element_prefix+'s'].pop();
+                    let main_modal_body = document.getElementById('main_modal_body');
+                    main_modal_body.innerHTML="";
+                    let c_body = modalComponentInformation(structure, idx_cat, idx_subcat, cell, graph, handleResize);
+                    main_modal_body.appendChild(c_body);
+                    const container = cell.getChildAt(element.element_prefix === 'attribute' ? 1 : 2);
+                    const lastCell = container.getChildAt(container.getChildCount()-1);
+                    graph.removeCells([lastCell]);
+
+                    //Handle Class Resize
+                    handleResize(null, {getProperty(_string) {return [cell];}});
+                }
+            });
+            input.appendChild(b1);
+            input.appendChild(b2);
+        } else {
+            alert("A problem occurred");
+        }
+
+        let td2 = document.createElement('td');
+        if(input !== null){
+            input.addEventListener('input', function(){
+                element.value = input.value;
+            });
+            if(element.value !== undefined && element.value !== null){
+                if(element.element_type === 'combo'){
+                    input.selectedIndex = element.value;
+                } else {
+                    input.value = element.value;
+                    input.innerHTML = element.value;
+                }
+            }
+            td2.appendChild(input);
+        }
+        tr.appendChild(td2);
+        table.appendChild(tr);
+    });
+    return table;
 }
 
 /* end util */
