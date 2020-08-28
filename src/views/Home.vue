b708e4b974bb797ee6ac8e5d7face2b8ebca8932
aolopezeafit <aolopezh@eafit.edu.co>
2020-08-28 08:41:56 -0500

    Integración proyecto integrador

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div class="border-bottom text-left"><h1 class="h2">{{ $t("home_variamos") }} (Moduino Version)</h1></div>
+    <div class="border-bottom text-left"><h1 class="h2">{{ $t("home_variamos") }}</h1></div>
     <div class="div-text-area">{{ $t("about_variamos_desc") }}
       <br /><br />
       <img src="@/assets/images/logo.png" height="160px" width="220px" />
